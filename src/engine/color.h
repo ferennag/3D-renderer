e2712276a881d5f4bae51db12edbd3f9a033d507
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2024-04-20 15:21:23 -0400

    RBB and RGBA macro

--- src/engine/color.h
+++ src/engine/color.h
@@ -9,3 +9,6 @@ u32 to_hex(f32 value);
 Color rgb(f32 r, f32 g, f32 b);
 
 Color rgba(f32 r, f32 g, f32 b, f32 a);
+
+#define RGB(r, g, b) (rgb((r), (g), (b)))
+#define RGBA(r, g, b, a) (rgba((r), (g), (b), (a)))
